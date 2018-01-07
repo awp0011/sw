4a3f1b91e1be37be05163c45d3982e02d6977db5
Michael Wang <qq_wp@126.com>
2018-01-07 15:47:22 +0800

    20180108

--- src/sw/pro/SDS_PRO_6_2/source2.java
+++ src/sw/pro/SDS_PRO_6_2/source2.java
@@ -5,37 +5,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class source2 {
-    private static int[][][] dp;
-    private static String magic;
-    private static String bridge1;
-    private static String bridge2;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        magic = br.readLine();
-        bridge1 = br.readLine();
-        bridge2 = br.readLine();
-
-        dp = new int[magic.length() + 1][bridge1.length() + 1][3];
+        String magic = br.readLine();
+        String angle = br.readLine();
+        String evils = br.readLine();
+        int[][][] dp = new int[magic.length() + 1][angle.length() + 1][3];
 
         for (int i = 1; i <= magic.length(); i++) {
-            for (int j = 1; j <= bridge1.length(); j++) {
-                for (int k = 1; k <= 2; k++) {//1:天使桥；  2：恶魔桥；
-                    if (i == 1) {
-                        dp[1][j][k] = dp[1][j - 1][k] + (isMatchMagic(i, j, k) ? 1 : 0);
-                    } else {
-                        dp[i][j][k] = dp[i][j - 1][k] + (isMatchMagic(i, j, k) ? dp[i - 1][j - 1][3 - k] : 0);
-                    }
-                    //if(k==2) System.out.print(dp[i][j][k]+" ");
+            for (int j = 1; j <= angle.length(); j++) {
+                for (int k = 1; k < 3; k++) {
+                    dp[i][j][k] = dp[i][j - 1][k] + (((k == 1) ? angle.charAt(j - 1) : evils.charAt(j - 1)) == magic.charAt(i - 1) ? ((i == 1) ? 1 : dp[i - 1][j - 1][3 - k]) : 0);
                 }
             }
-            //System.out.println();
         }
-        System.out.println(dp[magic.length()][bridge1.length()][1] + dp[magic.length()][bridge1.length()][2]);
+        System.out.println(dp[magic.length()][angle.length()][1] + dp[magic.length()][angle.length()][2]);
     }
 
-    private static boolean isMatchMagic(final int magic_index, final int bridge_index, final int brideg) {
-        return ((brideg == 1) ? bridge1.charAt(bridge_index - 1) : bridge2.charAt(bridge_index - 1)) == magic.charAt(magic_index - 1);
-    }
-    
 }
