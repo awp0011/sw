fe4fdb515c50f23d5e5e8b8124aff9bd23157a61
Michael Wong <peng05.wang@126.com>
2018-10-31 22:42:50 +0800

    20181030

--- src/sw/pro/armyline/Solution.java
+++ src/sw/pro/armyline/Solution.java
@@ -1,39 +1,38 @@
 package sw.pro.armyline;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Solution {
 
     static int[] cash = new int[10005];
-    static int[] data;
+    static int[] firstLine = new int[10005];
     static int MOD = 100000007;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = Integer.valueOf(sc.nextLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.valueOf(br.readLine());
         for (int i = 1; i <= T; i++) {
-            String[] temp = sc.nextLine().split(" ");
-            data = new int[temp.length + 1];
+            String[] temp = br.readLine().split(" ");
             for (int j = 1; j <= temp.length; j++) {
-                data[j] = Integer.valueOf(temp[j - 1]);
+                firstLine[j] = Integer.valueOf(temp[j - 1]);
             }
-            System.out.println("#" + i + " " + solve());
+            System.out.println("#" + i + " " + solve(temp.length));
         }
+        br.close();
     }
 
-    static int solve() {
-        Arrays.fill(cash, 1);
-        cash[data[data.length - 1] + 1] = 0;//
-        for (int i = data.length - 2; i > 0; i--) {
-            for (int j = data[i + 1]; j > 0; j--) {
-                if (j > data[i]) {
-                    cash[j - 1] += cash[j];
-                    cash[j - 1] %= MOD;
+    static int solve(final int armyLength) {
+        Arrays.fill(cash, 1, firstLine[armyLength] + 1, 1);
+        cash[firstLine[armyLength] + 1] = 0;//
+        for (int i = armyLength - 1; i > 0; i--) {
+            for (int j = firstLine[i + 1]; j > 0; j--) {
+                if (j > firstLine[i]) {
+                    cash[j - 1] = (cash[j - 1] + cash[j]) % MOD;
                     cash[j] = 0;
                 } else {
-                    cash[j] += cash[j + 1];
-                    cash[j] %= MOD;
+                    cash[j] = (cash[j] + cash[j + 1]) % MOD;
                 }
             }
         }
