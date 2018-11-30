985400e2ee91a8513ccf3d71011c3804d46dc4ca
Michael Wong <peng05.wang@126.com>
2018-11-30 20:20:06 +0800

    20181129-达人杯，第三天

--- src/sw/contest/second/D273048/Main.java
+++ src/sw/contest/second/D273048/Main.java
@@ -1,56 +1,101 @@
 package sw.contest.second.D273048;
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import static java.lang.Long.parseLong;
+import static java.lang.Math.abs;
+import static java.util.Collections.binarySearch;
+import static java.util.Comparator.comparing;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final List<Opt> del1 = new ArrayList<>(2000);
-    private static final List<Opt> del2 = new ArrayList<>(2000);
-    private static final List<Opt> ins1 = new ArrayList<>(2000);
-    private static final List<Opt> ins2 = new ArrayList<>(2000);
+    private static BufferedReader br;
+
 
     public static void main(String[] args) throws Exception {
-        exec(1);
-        exec(2);
-        System.out.println(isSame() ? 1 : 0);
-        br.close();
+        ArrayList<Opt> delList_1 = new ArrayList<>(2500);
+        ArrayList<Opt> delList_2 = new ArrayList<>(2500);
+        List<Opt> insList_1 = new ArrayList<>(2500);
+        List<Opt> insList_2 = new ArrayList<>(2500);
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        while (cnt < 80) {
+            if (exec(delList_1, insList_1)) {
+                exec(delList_2, insList_2);
+                System.out.println(isSame(delList_1, delList_2, insList_1, insList_2) ? 0 : 1);
+                delList_1.clear();
+                delList_2.clear();
+                insList_1.clear();
+                insList_2.clear();
+            } else {
+                break;
+            }
+            cnt++;
+        }
     }
 
-    private static void exec(int flag) throws Exception {
-        List<Opt> del = flag == 1 ? del1 : del2;
-        List<Opt> ins = flag == 1 ? ins1 : ins2;
-//Collections.binarySearch(del1, new DNA(), Comparator.comparing(Opt::getPos));
+    private static boolean exec(List<Opt> delList, List<Opt> insList) throws Exception {
         StringTokenizer st;
-        char next;
+        char next = 'Q';
         while (true) {
             st = new StringTokenizer(br.readLine());
+            if (st.countTokens() == 0) break;
             next = st.nextToken().charAt(0);
             if ('D' == next) {
+                Opt delOpt = new Opt();
+                delOpt.pos = parseLong(st.nextToken());
+                if (delIns(delOpt, insList)) continue;
+                int pos = binarySearch(delList, delOpt, comparing(Opt::getPos));
+                if (pos < 0) pos = abs(pos + 1);
+                delOpt.pos += pos;
+                delList.add(pos, delOpt);
 
             } else if ('I' == next) {
-
+                Opt insOpt = new Opt();
+                insOpt.pos = parseLong(st.nextToken());
+                insOpt.c = st.nextToken().charAt(0);
+                int pos = binarySearch(insList, insOpt, comparing(Opt::getPos));
+                if (pos < 0) pos = abs(pos + 1);
+                insList.add(pos, insOpt);
+                for (int i = pos + 1; i < insList.size(); i++) {
+                    insList.get(i).pos += 1;
+                }
             } else {
                 break;
             }
         }
+        return next != 'Q';
     }
 
-    private static boolean isSame() {
+    private static boolean delIns(Opt del, List<Opt> insList) {
+        int index = binarySearch(insList, del, comparing(Opt::getPos));
+        if (index < 0) return false;
+        else insList.remove(index);
+        return true;
+    }
+
+    private static boolean isSame(List<Opt> delList1, List<Opt> delList2, List<Opt> insList1, List<Opt> insList2) {
+        if (delList1.size() != delList2.size() || insList1.size() != insList2.size()) return false;
+        for (int i = 0; i < delList1.size(); i++) {
+            if (delList1.get(i) == null || delList2.get(i) == null) return false;
+            if (delList1.get(i).pos != delList2.get(i).pos) return false;
+        }
+        for (int i = 0; i < insList1.size(); i++) {
+            if (insList2.get(i) == null || insList1.get(i) == null) return false;
+            if (insList1.get(i).pos != insList2.get(i).pos) return false;
+            else if (insList1.get(i).c != insList2.get(i).c) return false;
+        }
         return true;
     }
 
     private static class Opt {
-        long pos;
-        char c;
-        Opt left;
-        Opt right;
+        long pos = 0;
+        char c = '0';
 
         long getPos() {
             return pos;
         }
     }
-
 }
