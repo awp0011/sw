56f1b243ce7241ea35aace06a5e715b07566de5e
Michael Wong <peng05.wang@126.com>
2018-12-02 15:54:48 +0800

    20181129-达人杯，第四天

--- src/sw/contest/second/D273048/MainV2.java
+++ src/sw/contest/second/D273048/MainV2.java
@@ -1,64 +1,65 @@
 package sw.contest.second.D273048;
 
-import java.util.Scanner;
 
-public class LongLongStrings {
-    private static final char NULL = '\0';
-    private static final long oo = (long)1e11;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Node a = doIt(scan), b = doIt(scan);
-        scan.close();
+import static java.lang.Long.*;
 
-        System.out.println(a.equals(b) ? "0" : "1");
+public class MainV2 {
+    private static final char NONE = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Opt first = execute(br), second = execute(br);
+        br.close();
+        System.out.println(first.equals(second) ? "0" : "1");
     }
 
-    private static Node doIt(Scanner scan) {
-        Node head = new Node(NULL);
-        head.next = new Node(1L, oo);
+    private static Opt execute(BufferedReader br) throws Exception {
+        Opt head = new Opt(NONE);
+        head.next = new Opt(1L,100_000_000_000L);
         head.next.prev = head;
-        head.next.next = new Node(NULL);
+        head.next.next = new Opt(NONE);
         head.next.next.prev = head.next;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (true) {
+            char optChar = st.nextToken().charAt(0);
+            if (optChar == 'E') break;
 
-        while(true) {
-            char c = scan.next().charAt(0);
-            if(c == 'E')
-                break;
-
-            long index = scan.nextLong() - 1L;
-            char ch = c == 'I' ? scan.next().charAt(0) : NULL;
-            Node n = head.next;
-            while(true) {
-                long len = Math.max(1L, n.end - n.start + 1L);
-                if(len <= index) {
-                    index -= len;
-                    n = n.next;
+            long pos = parseLong(st.nextToken()) - 1L;
+            char c = optChar == 'I' ? st.nextToken().charAt(0) : NONE;
+            Opt nextOpt = head.next;
+            while (true) {
+                long len = Math.max(1L, nextOpt.end - nextOpt.start + 1L);
+                if (len <= pos) {
+                    pos -= len;
+                    nextOpt = nextOpt.next;
                     continue;
                 }
 
-                if(ch == NULL)
-                    delete(n, index);
+                if (c == NONE)
+                    delete(nextOpt, pos);
                 else
-                    insert(n, index, ch);
+                    insert(nextOpt, pos, c);
 
                 break;
             }
+            st = new StringTokenizer(br.readLine());
         }
 
         return head;
     }
 
-    private static void delete(Node n, long index) {
-        Node prev = n.prev;
-        Node next = n.next;
+    private static void delete(Opt n, long index) {
+        Opt prev = n.prev;
+        Opt next = n.next;
 
-        if(n.start == -1L) {
+        if (n.start == -1L) {
             prev.next = next;
             next.prev = prev;
 
-            //re-unify segments if appropriate
-            if(prev.ch == NULL && next.ch == NULL && prev.end + 1 == next.start) {
+            if (prev.ch == NONE && next.ch == NONE && prev.end + 1 == next.start) {
                 prev.end = next.end;
                 prev.next = next.next;
                 next.next.prev = prev;
@@ -69,22 +70,22 @@ public class LongLongStrings {
             long start2 = n.start + index + 1L;
             long end2 = n.end;
 
-            Node first = start1 <= end1 ? new Node(start1, end1) : null;
-            Node last = start2 <= end2 ? new Node(start2, end2) : null;
+            Opt first = start1 <= end1 ? new Opt(start1, end1) : null;
+            Opt last = start2 <= end2 ? new Opt(start2, end2) : null;
 
-            if(first != null && last != null) {
+            if (first != null && last != null) {
                 prev.next = first;
                 first.prev = prev;
                 first.next = last;
                 last.prev = first;
                 last.next = next;
                 next.prev = last;
-            } else if(first != null) {
+            } else if (first != null) {
                 prev.next = first;
                 first.prev = prev;
                 first.next = next;
                 next.prev = first;
-            } else if(last != null) {
+            } else if (last != null) {
                 prev.next = last;
                 last.prev = prev;
                 last.next = next;
@@ -96,12 +97,12 @@ public class LongLongStrings {
         }
     }
 
-    private static void insert(Node n, long index, char ch) {
-        Node prev = n.prev;
-        Node next = n.next;
-        Node ins = new Node(ch);
+    private static void insert(Opt n, long index, char ch) {
+        Opt prev = n.prev;
+        Opt next = n.next;
+        Opt ins = new Opt(ch);
 
-        if(n.start == -1L) {
+        if (n.start == -1L) {
             prev.next = ins;
             ins.prev = prev;
             ins.next = n;
@@ -112,10 +113,10 @@ public class LongLongStrings {
             long start2 = n.start + index;
             long end2 = n.end;
 
-            Node first = start1 <= end1 ? new Node(start1, end1) : null;
-            Node last = start2 <= end2 ? new Node(start2, end2) : null;
+            Opt first = start1 <= end1 ? new Opt(start1, end1) : null;
+            Opt last = start2 <= end2 ? new Opt(start2, end2) : null;
 
-            if(first != null && last != null) {
+            if (first != null && last != null) {
                 prev.next = first;
                 first.prev = prev;
                 first.next = ins;
@@ -124,14 +125,14 @@ public class LongLongStrings {
                 last.prev = ins;
                 last.next = next;
                 next.prev = last;
-            } else if(first != null) {
+            } else if (first != null) {
                 prev.next = first;
                 first.prev = prev;
                 first.next = ins;
                 ins.prev = first;
                 ins.next = next;
                 next.prev = ins;
-            } else if(last != null) {
+            } else if (last != null) {
                 prev.next = ins;
                 ins.prev = prev;
                 ins.next = last;
@@ -147,19 +148,19 @@ public class LongLongStrings {
         }
     }
 
-    static class Node {
+    static class Opt {
         final long start;
         long end;
         char ch;
-        Node prev;
-        Node next;
+        Opt prev;
+        Opt next;
 
-        Node(long start, long end) {
+        Opt(long start, long end) {
             this.start = start;
             this.end = end;
         }
 
-        Node(char ch) {
+        Opt(char ch) {
             this.start = -1L;
             this.end = -1L;
             this.ch = ch;
@@ -167,17 +168,17 @@ public class LongLongStrings {
 
         @Override
         public boolean equals(Object obj) {
-            if(!(obj instanceof Node))
+            if (!(obj instanceof Opt))
                 return false;
 
-            Node a = this;
-            Node b = (Node)obj;
+            Opt a = this;
+            Opt b = (Opt) obj;
 
-            while(a != null) {
-                if(b == null)
+            while (a != null) {
+                if (b == null)
                     return false;
 
-                if(a.ch != b.ch || a.start != b.start || a.end != b.end)
+                if (a.ch != b.ch || a.start != b.start || a.end != b.end)
                     return false;
 
                 a = a.next;
--- src/sw/contest/second/D273048/Test.java
+++ src/sw/contest/second/D273048/Test.java
@@ -1,71 +1,12 @@
 package sw.contest.second.D273048;
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
-
 
 public class Test {
+    static char c;
     public static void main(String[] args) throws Exception {
-        LinkedList<Opt> delList_1 = new LinkedList<>();
-        LinkedList<Opt> delList_2 = new LinkedList<>();
-        LinkedList<Opt> insList_1 = new LinkedList<>();
-        LinkedList<Opt> insList_2 = new LinkedList<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt=22;
-        while (cnt>0) {
-            if (exec(delList_1, insList_1, br)) {
-                exec(delList_2, insList_2, br);
-                System.out.println(isSame(delList_1, delList_2, insList_1, insList_2) ? 0 : 1);
-                delList_1.clear();
-                delList_2.clear();
-                insList_1.clear();
-                insList_2.clear();
-            } else {
-                break;
-            }
-            cnt--;
-        }
-    }
-
-    private static boolean exec(List<Opt> delList, List<Opt> insList, BufferedReader br) throws Exception {
-        StringTokenizer st;
-        char next = 'Q';
-        while (true) {
-            st = new StringTokenizer(br.readLine());
-            if (st.countTokens() == 0) break;
-            next = st.nextToken().charAt(0);
-            if ('D' == next) {
-                Opt delOpt = new Opt();
-            } else if ('I' == next) {
-                Opt insOpt = new Opt();
-            } else {
-                break;
-            }
-        }
-        return next != 'Q';
+        System.out.println((int)c);
     }
 
 
-    private static boolean isSame(List<Opt> delList1, List<Opt> delList2, List<Opt> insList1, List<Opt> insList2) {
-        if (delList1.size() != delList2.size() || insList1.size() != insList2.size()) return false;
-        for (int i = 0; i < delList1.size(); i++) {
-            if (delList1.get(i) == null || delList2.get(i) == null) return false;
-            if (delList1.get(i).pos != delList2.get(i).pos) return false;
-        }
-        for (int i = 0; i < insList1.size(); i++) {
-            if (insList2.get(i) == null || insList1.get(i) == null) return false;
-            if (insList1.get(i).pos != insList2.get(i).pos) return false;
-            else if (insList1.get(i).c != insList2.get(i).c) return false;
-        }
-        return true;
-    }
-
-    private static class Opt {
-        long pos = 0;
-        char c = '0';
-    }
 }
