0366f0e382cbfbdf7e210c71f3e0090cf8d64c73
Michael Wong <peng05.wang@126.com>
2019-03-22 20:44:58 +0800

    真题：拓扑排序

--- src/sw/pro/real/exam/Solution20190321.java
+++ src/sw/pro/real/exam/Solution20190321.java
@@ -9,6 +9,8 @@ import static java.lang.Integer.parseInt;
 
 public class Solution20190321 {
     private static final Node[] map = new Node[100_003];
+    //为了进行字典顺序的拓扑排序，使用优先级队列进行排序，出队顺序即为  拓扑排序(字典顺序)
+    //Comparator.comparingInt(Node::getIndex) 优先级队列的比较器，语法糖
     private static final PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(Node::getIndex));
     private static final ArrayList<Integer> tpRet = new ArrayList<>();
 
@@ -30,7 +32,7 @@ public class Solution20190321 {
                 int B = parseInt(st.nextToken());
                 int D = parseInt(st.nextToken());
                 if (D == 1) {
-                    map[B].inDegree += map[A].AVO.add(B) ? 1 : 0;
+                    map[B].inDegree += map[A].AVO.add(B) ? 1 : 0;//去重，add方法返回true，插入成功，入度+1
                 }
 
             }
@@ -43,8 +45,7 @@ public class Solution20190321 {
             while (!pq.isEmpty()) {
                 tpRet.add(pq.peek().getIndex());
                 for (int i : pq.poll().AVO) {
-                    map[i].inDegree--;
-                    if (map[i].inDegree == 0) {
+                    if (--map[i].inDegree == 0) {//入度==0，进入优先队列
                         pq.offer(map[i]);
                     }
                 }
