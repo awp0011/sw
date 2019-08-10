9475d1a5a3446cc4bbf00dbb961ed5c81186ec59
Michael Wong <peng05.wang@126.com>
2019-08-11 00:04:08 +0800

    洛谷网校-练习题 新手村 1.洛谷的第一个任务 2.顺序与分支 3.循环！循环！循环！

--- src/sw/test/TempTest.java
+++ src/sw/test/TempTest.java
@@ -1,10 +1,26 @@
 package sw.test;
 
 
-class source {
+import java.util.Scanner;
+
+class Main {
+    private static final double l1 = 0.4463;
+    private static final double l2 = 0.4663;
+    private static final double l3 = 0.5663;
 
     public static void main(String[] args) {
-        System.out.println(('A'-'@'));
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        double fee = 0;
+        if (t >= 401) {
+            fee += l3 * (t - 400);
+            t = 400;
+        }
+        if(t>=151){
+            fee += l2 * (t - 150);
+            t = 150;
+        }
+        fee += l1 * t ;
+        System.out.printf("%.1f",fee);
     }
-
 }
