b15c92c2b6cacb33f00eb56eb7b1eafcffacad3c
Michael Wong <peng05.wang@126.com>
2019-08-11 12:17:30 +0800

    洛谷网校-练习题 新手村 5.简单字符串

--- src/sw/test/TempTest.java
+++ src/sw/test/TempTest.java
@@ -1,26 +1,11 @@
 package sw.test;
 
 
-import java.util.Scanner;
-
 class Main {
-    private static final double l1 = 0.4463;
-    private static final double l2 = 0.4663;
-    private static final double l3 = 0.5663;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        double fee = 0;
-        if (t >= 401) {
-            fee += l3 * (t - 400);
-            t = 400;
-        }
-        if(t>=151){
-            fee += l2 * (t - 150);
-            t = 150;
-        }
-        fee += l1 * t ;
-        System.out.printf("%.1f",fee);
+        System.out.println(('.' - '0'));
+        System.out.println(('/' - '0'));
+        System.out.println(('%' - '0'));
     }
 }
