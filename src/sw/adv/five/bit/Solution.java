0b1c47fc7ec50df0bf943dbaa890471159c35529
Michael Wang <wq_wp@126.com>
2019-08-11 20:13:50 +0800

    adv: 5 bit numbers

--- src/sw/adv/five/bit/Solution.java
+++ src/sw/adv/five/bit/Solution.java
@@ -1,8 +1,105 @@
 package sw.adv.five.bit;
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args)  {
+    private static long total;
+    private static int min, start, end, pos;
+    private static int[] minPos;
+    private static int[] intValues = new int[32];
+    private static final ArrayDeque<Integer> stock = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 32; i++) {
+            intValues[i] = 1 << i;
+        }
+        System.setIn(new FileInputStream("C:\\workspace\\idea\\sw\\src\\sw\\pro\\fiveone\\input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            start = sc.nextInt();
+            end = sc.nextInt();
+            min = 0;
+            getMin5Bit(start);
+            total = 0;
+            if (min > end) min = 0;
+            else sum5bit();
+            System.out.println("#" + t + " " + total + " " + min);
+        }
+
 
     }
 
+    private static void getMin5Bit(int a) {
+        minPos = new int[]{0, 1, 2, 3, 4};
+        if (a > 31) {
+            int index = 0;
+            while (a > 0) {
+                if ((a & 1) == 1) stock.add(index++);
+                else index++;
+                a >>= 1;
+            }
+            index = 4;
+            while (!stock.isEmpty() && index >= 0) {
+                minPos[index--] = stock.pollLast();
+            }
+            if (stock.size() > 0) {
+                next5bit();
+                stock.clear();
+            } else if (pos >= 0) {
+                for (int i = 1; i < 5; i++) {
+                    if (minPos[i] == minPos[i - 1]) minPos[i]++;
+                }
+                pos = firstZero();
+            }
+        }
+        min = getInt();
+    }
+
+    private static void next5bit() {
+        pos = firstZero();
+        minPos[pos]++;
+        init(pos);
+    }
+
+    private static int getInt() {
+        int intValue = 0;
+        for (int i = 0; i < 5; i++) {
+            intValue += intValues[minPos[i]];
+        }
+        return intValue;
+    }
+
+    private static int firstZero() {
+        int i = 0;
+        for (; i < 4; i++) {
+            if (minPos[i + 1] - minPos[i] > 1) break;
+        }
+        return i;
+    }
+
+    private static void init(int pos) {
+        for (int i = pos - 1; i >= 0; i--) {
+            minPos[i] = i;
+        }
+    }
+
+
+    private static void sum5bit() {
+        int next = 0;
+        total = min;
+        while (next <= end) {
+            total += next;
+            if (pos != 4 && minPos[pos + 1] - minPos[pos] > 1) {
+                minPos[pos]++;
+            } else {
+                next5bit();
+            }
+            next = getInt();
+        }
+    }
 }
--- src/sw/pro/fiveone/Test.java
+++ src/sw/pro/fiveone/Test.java
@@ -3,15 +3,12 @@ package sw.pro.fiveone;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayDeque;
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Test {
     private static long total;
-    private static int min, max;
-    private static HashSet<Integer> all = new HashSet<>();
-    private static int[] maxPos = new int[]{25, 26, 28, 29, 30};
-    private static int[] minPos = new int[]{0, 1, 2, 3, 4};
+    private static int min, start, end, pos;
+    private static int[] minPos;
     private static int[] intValues = new int[32];
     private static final ArrayDeque<Integer> stock = new ArrayDeque<>();
 
@@ -23,29 +20,14 @@ public class Test {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int t = 1; t <= T; t++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            start = sc.nextInt();
+            end = sc.nextInt();
             min = 0;
-            max = 0;
-            getMin5Bit(a);
-            getMax5Bit(b);
-
+            getMin5Bit(start);
             total = 0;
-
-            for (int i = 4; i >= 0; i--) {
-                int pix = 0;
-                for (int j = 0; j < i; j++) {
-                    pix += intValues[minPos[j]];
-                }
-                //find5bit(i, 4, pix, 32);
-            }
-            if (min > b) {
-                min = 0;
-                total = 0;
-            }
-            // System.out.println("#" + t + " " + max + " " + min);
-            System.out.println("#" + t + " " + min + " " + max);
-            all.clear();
+            if (min > end) min = 0;
+            else sum5bit();
+            System.out.println("#" + t + " " + total + " " + min);
         }
 
 
@@ -54,84 +36,69 @@ public class Test {
     private static void getMin5Bit(int a) {
         minPos = new int[]{0, 1, 2, 3, 4};
         if (a > 31) {
-            int pos = 0;
+            int index = 0;
             while (a > 0) {
-                if ((a & 1) == 1) stock.add(pos++);
-                else pos++;
+                if ((a & 1) == 1) stock.add(index++);
+                else index++;
                 a >>= 1;
             }
-            pos = 4;
-            while (!stock.isEmpty() && pos >= 0) {
-                minPos[pos--] = stock.pollLast();
+            index = 4;
+            while (!stock.isEmpty() && index >= 0) {
+                minPos[index--] = stock.pollLast();
             }
             if (stock.size() > 0) {
-                int p = firstZero(minPos);
-                minPos[p]++;
-                init(p, minPos);
+                next5bit();
                 stock.clear();
             } else if (pos >= 0) {
                 for (int i = 1; i < 5; i++) {
                     if (minPos[i] == minPos[i - 1]) minPos[i]++;
                 }
+                pos = firstZero();
             }
         }
-        for (int i = 0; i < 5; i++) {
-            min += intValues[minPos[i]];
-        }
+        min = getInt();
     }
 
-    private static int firstZero(int[] arr) {
+    private static void next5bit() {
+        pos = firstZero();
+        minPos[pos]++;
+        init(pos);
+    }
+
+    private static int getInt() {
+        int intValue = 0;
+        for (int i = 0; i < 5; i++) {
+            intValue += intValues[minPos[i]];
+        }
+        return intValue;
+    }
+
+    private static int firstZero() {
         int i = 0;
         for (; i < 4; i++) {
-            if (arr[i + 1] - arr[i] > 1) break;
+            if (minPos[i + 1] - minPos[i] > 1) break;
         }
         return i;
     }
 
-    private static void init(int pos, int[] arr) {
+    private static void init(int pos) {
         for (int i = pos - 1; i >= 0; i--) {
-            arr[i] = i;
+            minPos[i] = i;
         }
     }
 
-    private static void getMax5Bit(int a) {
-        maxPos = new int[]{0, 1, 2, 3, 4};
-        int pos = 0;
-        while (a > 0) {
-            if ((a & 1) == 1) stock.add(pos++);
-            else pos++;
-            a >>= 1;
-        }
-        pos = 4;
-        while (!stock.isEmpty() && pos >= 0) {
-            maxPos[pos--] = stock.pollLast();
-        }
-        if (pos > -1) maxPos[pos + 1]--;
-        stock.clear();
-        for (int i = 0; i < 5; i++) {
-            max += intValues[maxPos[i]];
-        }
-    }
 
-    private static void find5bit(int to, int pos, int sum, int lpos) {
-        if (to == 5) {
-            if (!all.contains(sum)) {
-                total += sum;
-                all.add(sum);
+    private static void sum5bit() {
+        int next = 0;
+        total = min;
+        while (next <= end) {
+            total += next;
+            if (pos != 4 && minPos[pos + 1] - minPos[pos] > 1) {
+                minPos[pos]++;
+            } else {
+                next5bit();
             }
-
-        } else {
-            for (int i = minPos[pos]; i <= maxPos[pos]; i++) {
-                if (i < lpos) {
-                    sum += intValues[i];
-                    find5bit(to + 1, pos - 1, sum, i);
-                    sum -= intValues[i];
-                } else {
-                    sum += intValues[i - 1];
-                    find5bit(to + 1, pos - 1, sum, i);
-                    sum -= intValues[i - 1];
-                }
-            }
+            next = getInt();
         }
     }
 }
