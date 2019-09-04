882569bd05f7843b6e292a537eec194cf231f260
Michael Wong <peng05.wang@126.com>
2019-09-04 23:15:06 +0800

    luogu stage2 4

--- src/sw/pro/Test.java
+++ src/sw/pro/Test.java
@@ -5,14 +5,20 @@ public class Test {
 
 
     public static void main(String[] args) {
-        int x = 131073,y = 171071;
-
-
-        while (x <= y) {
-            x = (x + 1) >> 1;
-            y = (y - 1) >> 1;
-            System.out.println(x+" "+y);
-        }
+        String[] num = new String[]{
+                "one", "two", "three", "four", "five", "six", "seven",
+                "eight", "nine", "ten", "eleven", "twelve", "thirteen",
+                "fourteen", "fifteen", "sixteen",
+                "seventeen", "eighteen", "nineteen", "twenty",
+                "a", "both", "another", "first", "second", "third"
+        };
+        int[] hash = new int[]{110182, 115276, 110339486,
+                3149094, 3143346, 113890, 109330445, 96505999, 3381426,
+                114717, -1300557247, -860970343, 1228536567, 565136640, -860088995,
+                2104147644, -1420708761, 1677195487, 380252812, -860968463, 97,
+                3029889, -851179773, 97440432, -906279820, 110331239};
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 1, 1, 2, 3};
+        System.out.println((int)'A');
     }
 
 }
