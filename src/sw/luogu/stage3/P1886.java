38afe147ea18ea99a9fd3bf12c48231bcf69469f
Michael Wong <peng05.wang@126.com>
2021-02-24 19:57:49 +0800

    提交 2021 02 22  考试题 练习

--- src/sw/luogu/stage3/P1886.java
+++ src/sw/luogu/stage3/P1886.java
@@ -1,53 +1,58 @@
 package sw.luogu.stage3;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
 import java.util.ArrayDeque;
 
 public class P1886 {
+    private static final ArrayDeque<Integer> que = new ArrayDeque<>();
+    private static final int[] d = new int[1000003];
+
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("C:\\Users\\peng0\\Documents\\Downloads\\testdata.in"));
-        System.setOut(new PrintStream("C:\\Users\\peng0\\Documents\\Downloads\\testdata.out"));
         StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        PrintWriter out = new PrintWriter(System.out);
         in.nextToken();
         int n = (int) in.nval;
         in.nextToken();
         int k = (int) in.nval;
-        ArrayDeque<Node> maxQue = new ArrayDeque<>();
-        ArrayDeque<Node> minQue = new ArrayDeque<>();
-        ArrayDeque<Node> st = new ArrayDeque<>();
-        for (int i = 0; i <= k; i++) st.addLast(new Node(i, 0));
-        int next;
-        StringBuilder maxStr = new StringBuilder();
-        StringBuilder minStr = new StringBuilder();
+
         for (int i = 0; i < n; i++) {
             in.nextToken();
-            next = (int) in.nval;
-            if (!maxQue.isEmpty() && i - maxQue.peekFirst().idx >= k) st.add(maxQue.pollFirst());
-            if (!minQue.isEmpty() && i - minQue.peekFirst().idx >= k) st.add(minQue.pollFirst());
-            while (!maxQue.isEmpty() && maxQue.peekLast().val < next) st.add(maxQue.pollLast());
-            while (!minQue.isEmpty() && minQue.peekLast().val > next) st.add(minQue.pollLast());
-            Node aNode = st.poll();
-            aNode.idx = i;
-            aNode.val = next;
-            maxQue.addLast(aNode);
-            minQue.addLast(aNode);
+            d[i] = (int) in.nval;
+        }
 
-            if (i >= k - 1) {
-                maxStr.append(maxQue.peekFirst().val).append(' ');
-                minStr.append(minQue.peekFirst().val).append(' ');
+        for (int i = 0; i < n; i++) {
+            addMinQueue(i, k);
+            if (i >= k - 1 && !que.isEmpty()) {
+                out.print(d[que.peekFirst()]);
+                out.print(' ');
             }
         }
-        System.out.println(minStr.toString());
-        System.out.println(maxStr.toString());
-    }
-
-    private static class Node {
-        int idx;
-        int val;
-
-        Node(final int i, final int v) {
-            idx = i;
-            val = v;
+        que.clear();
+        out.println("");
+        for (int i = 0; i < n; i++) {
+            addMaxQueue(i, k);
+            if (i >= k - 1 && !que.isEmpty()) {
+                out.print(d[que.peekFirst()]);
+                out.print(' ');
+            }
         }
+
+        out.flush();
     }
+
+    private static void addMaxQueue(int idx, int k) {
+        if (!que.isEmpty() && idx - que.peekFirst() >= k) que.pollFirst();
+        while (!que.isEmpty() && d[que.peekLast()] < d[idx]) que.pollLast();
+        que.addLast(idx);
+    }
+
+    private static void addMinQueue(int idx, int k) {
+        if (!que.isEmpty() && idx - que.peekFirst() >= k) que.pollFirst();
+        while (!que.isEmpty() && d[que.peekLast()] > d[idx]) que.pollLast();
+        que.addLast(idx);
+    }
+
 }
