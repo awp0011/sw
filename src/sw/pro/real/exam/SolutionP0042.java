d870165d9758e9bbcef69a262249528b960a36b2
Michael Wong <peng05.wang@126.com>
2021-03-10 22:32:40 +0800

    带有互斥边的 MST

--- src/sw/pro/real/exam/SolutionP0042.java
+++ src/sw/pro/real/exam/SolutionP0042.java
@@ -5,24 +5,30 @@ import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
 import java.util.*;
 
+
+/*
+ * 1.建立边与点的对应关系，HashMap<Integer,int[2]> mapping,int[2]为两个端点
+ * 2.简单并查集(点),MST时 需要使用 int[] nodeP
+ * 3.带权并查集(互斥边)，建立互斥关系 int[] edgeP, int[] flag(并查集的权，0 1表示互斥关系)
+ * 4.边权集合ArrayList<int[2]>，并进行升序排序，MST时 需要使用。int[2]为 [边序号，权值]
+ * 5.二进制枚举或者dfs，遍历所有互斥边的使用或不使用情况
+ * 6.MST 使用Kruskal算法实现，需要基于5的情况进行循环
+ * */
 public class SolutionP0042 {
     private static StreamTokenizer in;
-    private static PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-        @Override
-        public int compare(Integer o1, Integer o2) {
-            return edges[3][o1]-edges[3][o2];
-        }
-    });
-    private static final int[] p = new int[52];
-    //edges[0] 0不可用，1可用；edges[1]和edges[2] 线的端点; edges[3]: 权值
-    private static final int[][] edges = new int[4][202];
-    private static final int[][] pairs = new int[2][16];
-    private static final HashMap<Integer, Boolean> map = new HashMap<>();
-    private static int N, M, K;
+    private static final int[] nodeP = new int[52];
+    private static final int[] edgeP = new int[202];
+    private static final int[] flag = new int[202];
+    private static final boolean[] used = new boolean[202];//边 是否使用
+    private static final HashMap<Integer, int[]> map = new HashMap<>();
+    private static final int[][] w = new int[202][2];
+    private static final ArrayList<int[]> sorted = new ArrayList<>();
+    private static final ArrayList<Integer> parent = new ArrayList<>();
+    private static final BitSet mutex = new BitSet();
+    private static int N;
 
     private static void init() {
         in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-
     }
 
     private static int nextInt() throws Exception {
@@ -35,74 +41,128 @@ public class SolutionP0042 {
         int T = nextInt();
         for (int t = 1; t <= T; t++) {
             N = nextInt();
-            M = nextInt();
-            K = nextInt();
+            int M = nextInt();
+            int K = nextInt();
             for (int i = 1; i <= M; i++) {
-                edges[0][i] = i;
-                edges[1][i] = nextInt();
-                edges[2][i] = nextInt();
-                edges[3][i] = nextInt();
-                pq.add(i);
+                edgeP[i] = i;
+                flag[i] = -1;
+                used[i] = true;
+                int a = nextInt();
+                int b = nextInt();
+                map.put(i, new int[]{a, b});
+                w[i][0] = i;
+                w[i][1] = nextInt();
+                sorted.add(w[i]);
             }
+            sorted.sort(Comparator.comparingInt(o -> o[2]));
+            mutex.clear();
             for (int i = 0; i < K; i++) {
-                pairs[0][i] = nextInt();
-                pairs[1][i] = nextInt();
+                int a = nextInt();
+                int b = nextInt();
+                unionEdge(a, b);
+                mutex.set(a);
+                mutex.set(b);
             }
-            int total = (int) Math.pow(2, K);
+            parent.clear();
+            for (int i = 1; i < M; i++) {
+                if (mutex.get(i) && findEdge(i) == i) {
+                    parent.add(i);
+                }
+            }
+            //二进制枚举
+            int total = (int) Math.pow(2, parent.size());
+            total--;
             long min = Long.MAX_VALUE;
             for (int i = 0; i < total; i++) {
                 int binary = i, idx = 0;
                 while (binary > 0) {
-                    if ((binary ^ 1) == 0) {
-                        map.put(pairs[0][idx], true);
-                        map.put(pairs[1][idx], false);
-                    } else {
-                        map.put(pairs[0][idx], false);
-                        map.put(pairs[1][idx], true);
-                    }
+                    used[parent.get(idx)] = ((binary ^ 1) == 0);
                     binary >>= 1;
-                    idx++;
-                }
-                while (idx < K) {
-                    map.put(pairs[0][idx], true);
-                    map.put(pairs[1][idx], false);
                     idx++;
                 }
                 min = Math.min(min, MST());
             }
             if (min == Long.MAX_VALUE) min = -1;
             System.out.println("#" + t + " " + min);
+            map.clear();
+            sorted.clear();
         }
     }
 
+    //Kruskal
     private static long MST() {
         long mst = 0;
-        for (int i = 0; i < N; i++) p[i] = i;
+        for (int i = 0; i < N; i++) nodeP[i] = i;
         int cnt = 0;
-        Iterator<Integer> it = pq.iterator();
-        while (it.hasNext() && cnt != N - 1) {
-            int i = it.next();
-            if (map.get(edges[0][i])) {
-                if (union(edges[1][i], edges[2][i])) {
-                    cnt++;
-                    mst += edges[3][i];
-                }
+
+        for (int[] next : sorted) {
+            int edge_idx = next[0];
+            //互斥边是否使用判断
+            int p = findEdge(edge_idx);
+            if (p != edge_idx && flag[p] != flag[edge_idx] && used[p]) continue;
+
+
+            int a = map.get(edge_idx)[0];
+            int b = map.get(edge_idx)[1];
+            if (unionNode(a, b)) {
+                mst += next[1];
+                cnt++;
+                if (cnt == N - 1) break;
             }
         }
+
         if (cnt != N - 1) return Long.MAX_VALUE;
         return mst;
     }
 
-    private static int find(int c) {
-        if (p[c] == c) return c;
-        return p[c] = find(p[c]);
+    //带权并查集-find
+    private static int findEdge(int c) {
+        if (edgeP[c] == c) return c;
+        int p = edgeP[c];
+        edgeP[c] = findEdge(edgeP[c]);
+        flag[c] = reverse(flag[p]);
+        return edgeP[c];
     }
 
-    private static boolean union(int c1, int c2) {
-        int p1 = find(c1);
-        int p2 = find(c2);
+    //带权并查集-union
+    private static void unionEdge(int c1, int c2) {
+        int p1 = findEdge(c1);
+        int p2 = findEdge(c2);
+        if (p1 < p2) {
+            edgeP[p2] = p1;
+            if (flag[c1] == -1) flag[c1] = 0;
+            if (flag[c2] == -1) flag[c2] = (flag[c1] + 1) % 2;
+            if (flag[c1] == flag[c2]) {
+                flag[c2] = reverse(flag[c1]);//c1 c2 互斥
+                flag[p2] = reverse(flag[p2]);//自身反转
+            }
+        } else if (p1 > p2) {
+            edgeP[p1] = p2;
+            if (flag[c2] == -1) flag[c2] = 0;
+            if (flag[c1] == -1) flag[c1] = (flag[c2] + 1) % 2;
+            if (flag[c1] == flag[c2]) {
+                flag[c1] = reverse(flag[c2]);//c1 c2 互斥
+                flag[p1] = reverse(flag[p1]);//自身反转
+            }
+        }
+    }
+
+    //简单并查集-find
+    private static int findNode(int c) {
+        if (nodeP[c] == c) return c;
+        return nodeP[c] = findNode(nodeP[c]);
+    }
+
+    //简单并查集-union
+    private static boolean unionNode(int c1, int c2) {
+        int p1 = findNode(c1);
+        int p2 = findNode(c2);
         if (p1 == p2) return false;
-        p[p1] = p2;
+        nodeP[p1] = p2;
         return true;
+    }
+
+    private static int reverse(int src) {
+        return (src + 1) % 2;
     }
 }
